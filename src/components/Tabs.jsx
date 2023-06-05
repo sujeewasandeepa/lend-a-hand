30a2e219e7c24d24bd367d549678ef95f2bb9b9e
Sujeewa Sandeepa Kularathna <sujeewasandeepa@proton.me>
2023-06-05 11:23:04 +0530

    create tabs

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,41 @@
+import "../assets/Tabs.css"
+import { useState } from "react"
+
+export default function Tabs (props) {
+
+  const [adsActive, setAdsActive] = useState("is-active");
+  const [reqActive, setReqActive] = useState("");
+
+  const setActiveComp = props.setActiveComp;
+
+  const handleAdsClick = () => {
+    setAdsActive("is-active");
+    setReqActive("");
+    setActiveComp(0);
+  }
+
+  const handleReqClick = () => {
+    setReqActive("is-active");
+    setAdsActive("");
+    setActiveComp(1);
+  }
+
+  return (
+  <>
+  <div id="tabs-div" class="tabs is-toggle">
+    <ul>
+      <li className={`${adsActive}`} onClick={handleAdsClick}>
+        <a>
+          <span>Ads</span>
+        </a>
+      </li>
+      <li className={`${reqActive}`} onClick={handleReqClick}>
+        <a>
+          <span>Requests</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  </>
+  )
+}
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,28 @@
+import Tabs from "../components/Tabs";
+import { useState } from "react";
+
 export default function Home () {
+  // if activeComp is 0 consider Ads is active 
+  // if it is 1 consider Reqs is active
+  const [activeComp, setActiveComp] = useState(0);
+
     return (
         <>
-            <h1>From home</h1>
+            <h1 className="title is-2 has-text-centered mt-3">Handy Hero</h1>
+
+            <div className="has-text-centered">
+              <Tabs setActiveComp={setActiveComp}/>
+              {activeComp==0 ?
+                <div>
+                  Ads
+                </div>
+                :
+                <div>
+                  reqs
+                </div>
+              }
+            </div>
+                         
         </>
     )
 }
